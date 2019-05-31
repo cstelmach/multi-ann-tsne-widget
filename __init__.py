2849c4873759b50a60dd7ede52826d6ef71f7ab3
Christian Stelmach <chrisp.stel@gmail.com>
2019-05-31 14:08:25 +0200

    Complete change in approach
    Instead of modifying the whole wdiget, only the building of the KNNIndex, part of the perplexity based
    caclulation of nearest neighbors will be modified. This target change allows the forgoing of creating a
    pip package since there is no need for compilation.
    
    In process of the Orange update the whole project has been mistakenly deleted.
    This allows for a restart and brings thus a change in folder structure.
    
    The current task is to implement the ann-algorithm-api's in the KNNIndex build.

---
+++ __init__.py
@@ -0,0 +1,13 @@
+"""
+Widgets from Evaluate category
+
+"""
+NAME = "Bakk Stelmach"
+
+DESCRIPTION = "Bachelor Thesis Multi ANN t-SNE"
+
+BACKGROUND = "#CC6262"
+
+ICON = "icons/Category-Evaluate.svg"
+
+PRIORITY = 5
